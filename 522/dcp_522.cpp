7a92a7b35c6ef71c988a6fc238bef33451fda6bc
itsmohitj <itsmohitj@gmail.com>
2020-07-20 12:23:16 +0530

    added 522 solution

---
+++ 522/dcp_522.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std; 
+void allOccurrence(string str, string substring){ 
+    for (int i = 0; i < str.length(); i++) { 
+        if (str.substr(i, substring.length()) == substring) { //substr is a predefined function which takes two arguments, first is the position from where to start and second is the length of the substring
+	       // It returns a new string which is substring of the other string
+		// for example if string str is "example", then str.substr(2,3) will return "amp".
+            cout << i << " "; 
+        } 
+    } 
+} 
+
+int main()
+{
+    string str ="abracadabra";
+    string substring = "abr";
+    allOccurrence(str, substring);
+    return 0;
+}
