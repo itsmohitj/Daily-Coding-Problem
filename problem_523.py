95a500c93d5b438fef1a4e4ca263080e01460715
itsmohitj <itsmohitj@gmail.com>
2020-09-15 07:22:21 +0530

    Added Problem 523 Statement

--- problem_523.py
+++ problem_523.py
@@ -1,3 +1,13 @@
+"""
+This problem was asked by Jane Street.
+
+Given integers M and N, write a program that counts how many positive integer pairs (a, b) satisfy the following conditions:
+
+a + b = M
+a XOR b = N
+"""
+
+
 def plusXorPairs(m,n):
     count=0
     for i in range(1,m):
@@ -8,3 +18,6 @@ def plusXorPairs(m,n):
     return count
 
 print(plusXorPairs(11,11))
+
+# Time Complexity = O(m)
+# Space Complexity = O(1)
